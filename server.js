9506d50304124b2de0b5e51abf380eeb36166ace
renatodeyvson <renatodeyvson@gmail.com>
2017-11-28 02:12:15 -0200

    gambs (get to post)

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 //imports
 var express = require('express'),
     app = express(),
-    http = require('http').Server(app);
+    http = require('http').Server(app),
+    request = require('request');
 
 //config
 app.use(express.static(__dirname + '/view'));
+
+//gambs
+app.get('/:nome/:email/:senha', (req, res) => {
+    request.post({
+        uri: 'http://localhost:8881/',
+        method: 'post',
+        json: {
+            nome: req.params.nome,
+            email: req.params.email,
+            senha: req.params.senha
+        }
+    }, (err, data) => {
+        if(err) res.status(500).send({ err: err, message: 'Erro interno do sistema.' });
+        else {
+
+            if(data.statusCode != 200) res.status(500).send(data.body);
+            else {
+                
+                console.log(data.body);
+                res.status(200).send(data.body);
+
+            }
+
+        }
+    });
+});
 
 //listen
 var port = process.env.PORT || 8882;
